7385bf820478bfaf7406feb7aa436a8d757c6a96
IchigoPres <163737491+IchigoPres@users.noreply.github.com>
2024-03-17 21:23:33 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
-
+function id(elementId) { return document.getElementById(elementId) };
+let json;
+async function thief(link) {
+  await fetch(link).then(r => r.json()).then(data => json = data);
+  console.log(json);
+};
+thief('../json/users.json');
